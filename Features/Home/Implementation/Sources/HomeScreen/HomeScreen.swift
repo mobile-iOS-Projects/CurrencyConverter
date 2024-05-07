056791c5b0b8b8297b53c41a73659a29f9793d38
Sergey <sergeykin1997@mail.ru>
2024-05-07 09:19:02 +0300

    test

--- Features/Home/Implementation/Sources/HomeScreen/HomeScreen.swift
+++ Features/Home/Implementation/Sources/HomeScreen/HomeScreen.swift
@@ -10,17 +10,8 @@ import HomeAPI
 
 class HomeScreen: HomeAPI {
     func test2() {
-        print("test")
-        
-        
     }
 
-    
-    
     func test() {
-        print("test")
     }
-    
-    
-    
 }
