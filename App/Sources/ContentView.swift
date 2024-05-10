46cee715eaaafcc3aae38fe3544c446cfc783a34
Sergey <sergeykin1997@mail.ru>
2024-05-10 16:53:18 +0300

    vfvfdvfdd

--- App/Sources/ContentView.swift
+++ App/Sources/ContentView.swift
@@ -5,7 +5,7 @@ public struct ContentView: View {
     public init() {}
 
     public var body: some View {
-        Text("Hello, World Tuist!")
+        Text("Hello yyy, World Tuist!")
             .padding()
     }
 }
