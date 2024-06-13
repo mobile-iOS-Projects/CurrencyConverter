8288c95bf1b575741796fcfdb6862cc8652dcda5
Mikita Halitski <nikita.halitski97@gmail.com>
2024-06-13 15:47:29 +0200

    added possibility to select custom fonts

--- Features/Conversion/Implementation/Sources/ConversionScreen/ConversionScreen.swift
+++ Features/Conversion/Implementation/Sources/ConversionScreen/ConversionScreen.swift
@@ -5,12 +5,13 @@
 //  Created by Siarhei Runovich on 3.06.24.
 //
 
+import CurrencyCoreUI
 import SwiftUI
 
 public struct ConversionScreen: View {
-    
+
     public init() {}
-    
+
     public var body: some View {
         VStack {
             Rectangle().foregroundStyle(Color.red)
